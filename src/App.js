f0445c386cd0deb8f966d85aec9e1907ec068f6e
AzamatRaimbekov <arturfunks88@gmail.com>
2022-05-11 13:50:12 +0300

    cheking

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
+import AdminContext from "./contexts/AdminContext";
+import ClinentContext from "./contexts/ClinentContext";
+import Navigation from "./Navigation";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <ClinentContext>
+      <AdminContext>
+        <Navigation />
+      </AdminContext>
+    </ClinentContext>
+  );
 };
 
 export default App;
